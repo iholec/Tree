47345fdad9df0b2f94e43d370f326d48d647a425
Chaoskatze <irene.holec@technikum-wien.at>
2018-04-23 15:18:06 +0200

    Studd

--- src/DataController.java
+++ src/DataController.java
@@ -39,7 +39,7 @@ class DataController {
 		smsText = smsText.replaceAll("[+]*\\d{5,}", "numberReplacement");//5 is minimum phone number length
 		System.out.println(smsText);
 		System.out.println("Replacing weird stars of sms : \n" + smsText);
-		smsText = smsText.replaceAll("[*]{5,}", "numberReplacement");//5 is minimum phone number length/more than 5 stars without stuff always spam
+		smsText = smsText.replaceAll("[*]{5,}", "weirdStarsReplacement");//5 is minimum phone number length/more than 5 stars without stuff always spam
 		System.out.println(smsText);
 		System.out.println("Replacing emails of sms : \n" + smsText);
 		smsText = smsText.replaceAll("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])", "emailReplacement");  //this is fine
